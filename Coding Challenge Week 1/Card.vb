829e0c18cb5b1f6e6ea89571fafb27e706dc8851
Callum Newlands <callumnewlands@googlemail.com>
2018-01-04 01:12:05 +0000

    Moved Card Class

---
+++ Coding Challenge Week 1/Card.vb
@@ -0,0 +1,32 @@
+﻿Enum Suits
+    Spades
+    Hearts
+    Clubs
+    Diamonds
+End Enum
+
+Enum Values
+    One
+    Two
+    Three
+    Four
+    Five
+    Six
+    Seven
+    Eight
+    Nine
+    Ten
+    Jack
+    Queen
+    King
+End Enum
+
+Class Card
+    Property Suit As Suits
+    Property Value As Values
+    ReadOnly Property Name As String
+        Get
+            Return Value.ToString() + " of " + Suit.ToString()
+        End Get
+    End Property
+End Class
