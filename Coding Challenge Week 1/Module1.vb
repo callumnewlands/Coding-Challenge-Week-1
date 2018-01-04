2ee24c41bf7c308323876e4c5524fd583c525814
Callum Newlands <callumnewlands@googlemail.com>
2018-01-04 01:09:15 +0000

    Added Card class

--- Coding Challenge Week 1/Module1.vb
+++ Coding Challenge Week 1/Module1.vb
@@ -1,7 +1,40 @@
-﻿Module Module1
+﻿Enum Suits
+    Spades
+    Hearts
+    Clubs
+    Diamonds
+End Enum
+
+Enum Values
+    One
+    Two
+    Three
+    Four
+    Five
+    Six
+    Seven
+    Eight
+    Nine
+    Ten
+    Jack
+    Queen
+    King
+End Enum
+
+Class Card
+    Property Suit As Suits
+    Property Value As Values
+    ReadOnly Property Name As String
+        Get
+            Return Value.ToString() + " of " + Suit.ToString()
+        End Get
+    End Property
+End Class
+
+Module Module1
 
     Sub Main()
-        Console.WriteLine("SNAP!")
+
     End Sub
 
 End Module
